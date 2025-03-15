1f8b4531f9242bde03ccbc3c7c8fe905406b6f95
aashish-65 <aashishprasad65@gmail.com>
2025-03-16 00:44:43 +0530

    Change location of index.html

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "public",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "public/index.html", // Define entry file
+    },
+  },
   server: {
     allowedHosts: [
-      '1502-2409-40e1-6-3e66-55f5-f9c9-3323-f466.ngrok-free.app',
-      'localhost',
+      "1502-2409-40e1-6-3e66-55f5-f9c9-3323-f466.ngrok-free.app",
+      "localhost",
     ],
   },
-})
+});
